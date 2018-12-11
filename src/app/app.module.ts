11ddc4c45df89f4308fee62c17ca54d4e88a5780
USQA <usqa@USQAs-MacBook-Air.local>
2018-12-11 14:14:44 +0800

    # Changes for lession 1.5: a) Componentize the app.component.html page into multiple components: header, footer, content, menu, sidebar. b) Add book/booklist component and initialize its html page with 'Bordered Table' and 'Search Tool'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BooklistComponent } from './book/booklist/booklist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    MenuComponent,
+    SidebarComponent,
+    BooklistComponent
   ],
   imports: [
     BrowserModule,
