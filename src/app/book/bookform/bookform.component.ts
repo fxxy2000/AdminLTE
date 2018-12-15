cb8ca7462525994bc2c6297a8960ebee6ade8525
USQA <usqa@USQAs-MacBook-Air.local>
2018-12-15 23:12:39 +0800

    # Changes for Chapter 6 : a) Create FormModule using FormBuilder. b) Bind FormModule to form. c) Add validation criteria to form. d) Update book with changes made on page when save button was clicked.

--- src/app/book/bookform/bookform.component.ts
+++ src/app/book/bookform/bookform.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BookService, Book } from '../book.service';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-bookform',
@@ -9,6 +10,8 @@ import { BookService, Book } from '../book.service';
 })
 export class BookformComponent implements OnInit {
 
+  private formModule : FormGroup;
+  private categories = ['IT', 'Financial', 'Internet'];
   private mBook : Book;
   private rating : number;
 
@@ -18,6 +21,18 @@ export class BookformComponent implements OnInit {
     let id = this.route.snapshot.params['id'];
     this.mBook = this.bookService.getBookById(id);
     this.rating = this.mBook.rating;
+
+    let formBuilder = new FormBuilder();
+    this.formModule = formBuilder.group({
+      bookName : [this.mBook.name, [Validators.required, Validators.minLength(3)]],
+      bookPrice : [this.mBook.price, [Validators.required]],
+      bookDesc : [this.mBook.desc],
+      bookCategory : formBuilder.array([
+        [this.mBook.categories.includes(this.categories[0])],
+        [this.mBook.categories.includes(this.categories[1])],
+        [this.mBook.categories.includes(this.categories[2])]
+      ], categoryValidator)
+    });
   }
 
   updateRating(newRating : number) {
@@ -29,9 +44,41 @@ export class BookformComponent implements OnInit {
   }
 
   save() {
+    let newCategories = [];
+    for(var i=0; i<3; i++){
+      if(this.formModule.value.bookCategory[i]) {
+        newCategories.push(this.categories[i]);
+      }
+    }
+
     if(this.mBook.rating != this.rating) {
       this.mBook.rating = this.rating;
     }
+
+    if(this.mBook.name != this.formModule.value.bookName) {
+      this.mBook.name = this.formModule.value.bookName;
+    }
+
+    if(this.mBook.price != this.formModule.value.bookPrice) {
+      this.mBook.price = this.formModule.value.bookPrice;
+    }
+
+    if(this.mBook.desc != this.formModule.value.bookDesc) {
+      this.mBook.desc = this.formModule.value.bookDesc;
+    }
+
+    if(this.mBook.categories != newCategories) {
+      this.mBook.categories = newCategories;
+    }
+
     this.router.navigateByUrl('/booklist');
   }
+}
+
+export function categoryValidator(categories : FormArray) {
+  let isAnyCategorySelectled = false;
+  categories.controls.forEach(category => {
+    isAnyCategorySelectled = isAnyCategorySelectled || category.value;
+  });
+  return isAnyCategorySelectled ? null : {categories : true};
 }
